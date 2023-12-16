5dcd4ac21713b9f3ab6c0d7345d25239feaf2e09
JEFTEDARIEL123 <jeftedariel3@gmail.com>
2023-12-15 19:27:05 -0600

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,36 @@ import logo from './assets/logo.png'
 function App() {
   
   const uppy = new Uppy({
-    autoProceed: true,
+    debug: true,
+    autoProceed: false,
     restrictions: {
-      maxFileSize: 1000000, 
-      
+      maxFileSize: null,
+      minNumberOfFiles: 1,
 
-    },
-  });
+    }})
 
-  uppy.use(XHRUpload, {
-    endpoint: 'URL',
-  });
+   uppy.use(XHRUpload, {
+     endpoint: 'http://localhost:8080/upload', 
+     formData: true,
+      });
+    
+  fetch('http://localhost:8080/listFiles')
+  .then(response => response.json())
+  .then(data => {
+   console.log('Lista de archivos:', data);
+  })
+  .catch(error => console.error('Error al obtener la lista de archivos:', error));
+
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="" target="_blank">
           <img src={logo} className="logo" alt="logo" />
         </a>
       <h1>Cloudy</h1>
       <Dashboard uppy={uppy} />
+      <h2></h2>
     </div>
 
     </>
